f3e6b19672ce6e6b1200380b365b8836696d75ff
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-06-01 21:29:20 +0530

    update backend series cmplt

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -9,9 +9,9 @@ import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this  file for check the user is auth or not
 
 router.route("/:videoId").get(getVideoComments).post(addComment);
 router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
 
-export default router
+export default router 
