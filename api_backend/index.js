fb116efed68b4c364047e20d1e5fa596e2636814
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-07-11 22:29:30 +0530

    api_hub

---
+++ api_backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/api", (req, res) => {
+    res.send(
+        // dummy user info
+        {
+            name: "test user",
+            age: 25,
+            city: "Pune",
+            country: "India",
+            state: "Maharashtra",
+            pincode: 411001,
+            phone: 1234567890,
+        }
+    )
+})
+app.listen(3001);
+
