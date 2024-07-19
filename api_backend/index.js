57e7e2e2835b0b880c743c282b5f1dd27a053f18
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-07-19 12:00:26 +0530

    update run command

--- api_backend/index.js
+++ api_backend/index.js
@@ -1,13 +1,34 @@
 const express = require('express');
 
 const app = express();
+const packageJsonApi = {
+    "name": "api_backend",
+    "version": "1.0.0",
+    "description": "api hub",
+    "main": "index.js",
+    "scripts": {
+        "test": "dev ",
+        "dev": "node index.js",
+        "start": "node index.js",
+        "build": "node index.js"
+    },
+    "keywords": [
+        "api"
+    ],
+    "author": "swapnil ingle",
+    "license": "ISC",
+    "dependencies": {
+        "express": "^4.19.2"
+    }
+}
+
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.get("/api", (req, res) => {
     res.json(
-        
+
         {
             name: "test user",
             age: 25,
@@ -18,6 +39,9 @@ app.get("/api", (req, res) => {
             phone: 1234567890,
         }
     )
+})
+app.get("/api/json", (req, res) => {
+    res.json(packageJsonApi)
 })
 app.listen(3001);
 
