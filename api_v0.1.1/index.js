b50b1c37bdc3ceafd20e8e12e7ca7c3539ffa637
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-07-19 21:13:45 +0530

    cors update

--- api_v0.1.1/index.js
+++ api_v0.1.1/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-
+const corsRes = {
+    origin: [
+        'http://localhost:3001',
+    ]
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
@@ -16,6 +20,6 @@ app.listen(3000, () => {
 });
 
 import RandomUserRoutes from './src/routes/randomUser/randomUser.router.js';
-app.use('/v0.1/random_user', RandomUserRoutes)
+app.use('/v0.1/random_user',  cors(corsRes) , RandomUserRoutes)
 import RandomImageRoutes from './src/routes/randomImages/randomImage.router.js';
-app.use('/v0.1/random_image', RandomImageRoutes)
+app.use('/v0.1/random_image',  cors(corsRes) , RandomImageRoutes)
