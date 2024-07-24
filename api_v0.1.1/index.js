3af3d84751145289f25a10db0173fb5ca8e46d55
CodersHub-in <ingleswapnil2004@gmail.com>
2024-07-24 14:46:38 +0530

    bug fix

--- api_v0.1.1/index.js
+++ api_v0.1.1/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { homePageApi } from './src/controllers/homePageApi/homePageApi.controller.js';
 
 const app = express();
 const corsRes = {
