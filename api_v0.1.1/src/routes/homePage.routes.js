5967c8cdf3f8a2bdf52f3e32aa39362b5a398363
CodersHub-in <ingleswapnil2004@gmail.com>
2024-07-24 14:52:19 +0530

    b f 1

--- api_v0.1.1/src/routes/homePage.routes.js
+++ api_v0.1.1/src/routes/homePage.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { homePageApi } from "../controllers/homePageApi/homePageApi.controller";
+import { homePageApi } from "../controllers/homePageApi/homePageApi.controller.js";
 
 const router = Router();
 
