799b4e5ec1ec06d03b3bcb67abe92a6cebb41ee7
CodersHub-in <ingleswapnil2004@gmail.com>
2024-07-25 17:08:59 +0530

    bug hunt

--- api_v0.1.1/src/lib/data/api/v1.0/random_user/country/US/index.js
+++ api_v0.1.1/src/lib/data/api/v1.0/random_user/country/US/index.js
@@ -1,27 +1,8 @@
-import  ReadTextFile from "../../../../../../func/fetchTXTfilesAsArray.js"
-import { dirname , join } from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs/promises';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const readTextFile = async (fileName , path = __dirname) => {
-    const filePath = join(path, fileName);
-    try {
-        const data = await fs.readFile(filePath, 'utf-8');
-        const lines = data.split('\n').map(line => line.trim()).filter(line => line.length > 0);
-        return lines;
-    } catch (error) {
-        console.error(`Error reading file: ${error}`);
-        return [];
-    }
-}
-
-const firstName =  await readTextFile('firstName.txt' , __dirname)
-const lastName = await readTextFile('lastName.txt' , __dirname)
-const streets = await readTextFile('streets.txt' , __dirname)
-const states = await readTextFile('states.txt' , __dirname)
-const citys = await readTextFile('citys.txt' , __dirname)
+import firstName from './firstName.js'
+import lastName from './lastName.js'
+import streets from './streets.js'
+import states from './states.js'
+import citys from './citys.js'
 
 export {
     firstName, 
