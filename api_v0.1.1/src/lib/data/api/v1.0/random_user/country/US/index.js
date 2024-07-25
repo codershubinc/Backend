4c1df7d253483163d490aa37ee2cfdda10082e7a
CodersHub-in <ingleswapnil2004@gmail.com>
2024-07-25 16:27:49 +0530

    bug hunt

--- api_v0.1.1/src/lib/data/api/v1.0/random_user/country/US/index.js
+++ api_v0.1.1/src/lib/data/api/v1.0/random_user/country/US/index.js
@@ -1,9 +1,21 @@
-import  readTextFile from "../../../../../../func/fetchTXTfilesAsArray.js"
-import { dirname } from 'path';
+import  ReadTextFile from "../../../../../../func/fetchTXTfilesAsArray.js"
+import { dirname , join } from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const readTextFile = async (fileName , path = __dirname) => {
+    const filePath = join(path, fileName);
+    try {
+        const data = await fs.readFile(filePath, 'utf-8');
+        const lines = data.split('\n').map(line => line.trim()).filter(line => line.length > 0);
+        return lines;
+    } catch (error) {
+        console.error(`Error reading file: ${error}`);
+        return [];
+    }
+}
 
 const firstName =  await readTextFile('firstName.txt' , __dirname)
 const lastName = await readTextFile('lastName.txt' , __dirname)
